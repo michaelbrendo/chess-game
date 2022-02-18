e809009890f1fc3a247fa05bb9abde22fae74b37
michaelbrendo <23271123+michaelbrendo@users.noreply.github.com>
2022-02-18 17:07:32 -0300

    Create and implement the position class and use gitignore

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
+
+        Position position = new Position(2, 3);
+        System.out.println(position);
 
     }
 }
