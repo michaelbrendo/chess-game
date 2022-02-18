2efc0eb5934f92d17011ac1e70f4e3f7280f34c9
michaelbrendo <23271123+michaelbrendo@users.noreply.github.com>
2022-02-18 18:11:09 -0300

    Implement the chess layer and show the board in the terminal Topics: Enumerations Encapsulation / Access Modifiers Inheritance Downcasting Static members Layers pattern

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,16 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
+
+import javax.swing.*;
 
 public class Program {
     public static void main(String[] args) {
 
-        Board board = new Board(8,8);
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
