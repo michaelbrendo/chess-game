2d6332ef5e15a68fbc27e36af4153a4aebf1e026
michaelbrendo <23271123+michaelbrendo@users.noreply.github.com>
2022-02-22 17:49:03 -0300

    Implements chess pieces and places on the board: Inheritance, overriding and polymorphism (method toString)

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,17 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -14,9 +19,14 @@ public class ChessMatch {
 
         for (int i = 0; i < board.getRows(); i++) {
             for (int j = 0; j < board.getColumns(); j++) {
-                matrix[i][j] = (ChessPiece) board.piece(i,j);
+                matrix[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return matrix;
+    }
+
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(1, 1));
+        board.placePiece(new King(board, Color.BLACK), new Position(3, 2));
     }
 }
