495019ea9560b7c5469e8b951916a7cb4a6e4531
michaelbrendo <23271123+michaelbrendo@users.noreply.github.com>
2022-02-28 19:16:43 -0300

    Create ChessException and ChessPosition, implement placeNewPiece method and remove imports Exceptions, Encapsulation, Constructors(Exception), overriding, static members, layers pattern.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,8 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Piece;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -25,8 +23,13 @@ public class ChessMatch {
         return matrix;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(1, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(3, 2));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
